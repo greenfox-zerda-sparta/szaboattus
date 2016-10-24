5a3f713543932491dbaedd2d665086e94002cb5c
Szabó Attus <szabo.attila.10@gmail.com>
2016-10-24 11:57:57 +0200

    1-5 task done

---
+++ w2/d1/d1_05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+// cmath include!
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int e = 8;
+	// please cube of e's value
+	e = pow (e,3);
+	cout << e;
+	return 0;
+}
