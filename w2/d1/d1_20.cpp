2ce7848f7eec547107a31ff683e7bfeea8e3fe41
Szabó Attus <szabo.attila.10@gmail.com>
2016-10-24 18:08:04 +0200

    20 task fixed

--- w2/d1/d1_20.cpp
+++ w2/d1/d1_20.cpp
@@ -8,7 +8,7 @@ int main() {
 	// But for multiples of three print "Fizz" instead of the number
 	// and for the multiples of five print "Buzz".
 	// For numbers which are multiples of both three and five print "FizzBuzz".
-	for (int i=1 ; i<100; i++) {
+	for (int i=1 ; i<=100; i++) {
 		if ((i%3 ==0) && (i%5 ==0)) {
 			cout << "FizzBuzz" << endl;
 		}
