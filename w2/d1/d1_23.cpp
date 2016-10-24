56b2cf50a1efb301bbb268e4479519b210da51d5
Szabó Attus <szabo.attila.10@gmail.com>
2016-10-24 16:24:41 +0200

    22 25 tasks finish

---
+++ w2/d1/d1_23.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int ab = 123;
+	int credits = 100;
+	bool is_bonus = 0;
+	// if credits are at least 50,
+	// and is_bonus is False decrement ab by 2
+	// if credits are smaller than 50,
+	// and is_bonus is False decrement ab by 1
+	// if is_bonus is True ab should remain the same
+	if (credits >= 50 && is_bonus == 0){
+		ab=ab-2;
+	}
+	else if (credits<50 && is_bonus == 0){
+		ab=ab-1;
+	}
+	cout << ab;
+	return 0;
+}
---
+++ w2/d1/d1_24.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int ac = 8;
+	int time = 120;
+	string out = "";
+	// if ac is dividable by 4
+	// and time is not more than 200
+	// set out to 'check'
+	// if time is more than 200
+	// set out to 'Time out'
+	// otherwise set out to 'Run Forest Run!'
+	if (ac%2 ==0 && time <= 200){
+		cout << "check";
+	}
+	else if (time > 200){
+		cout << "Time out";
+	}
+	else {
+		cout << "Run Forest Run!";
+	}
+	return 0;
+}
