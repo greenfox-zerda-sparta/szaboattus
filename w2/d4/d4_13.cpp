01f7dde5cb690a8052419a527d5e54bf83e76598
Szabó Attus <szabo.attila.10@gmail.com>
2016-10-27 20:52:49 +0200

    task 13 random diceside

--- w2/d4/d4_13.cpp
+++ w2/d4/d4_13.cpp
@@ -1,35 +1,49 @@
-//============================================================================
-// Name        : d4_13.cpp
-// Author      : Programming first
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
 int main() {
-  int dice[] = {1, 2, 3, 4, 5, 6};
-  int length = sizeof(dice) / sizeof(int);
-  int combinations = 0;
-  int results [length+length] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  int sum = 0;
-  int results_numb = sizeof(results) / sizeof(int);
 
-  for (int i = 0; i < length; i++) {
-    for (int j = 0; j < length; j++) {
+
+  //request data
+  int dicenumber, diceside;
+  cout << "Hány dobókocka?" << endl;
+  cin >> dicenumber;
+  cout << "Hány oldaluk van?" << endl;
+  cin >> diceside;
+  cout << dicenumber<< " db dobókockánk van, és " << diceside << " oldala van egy kockának. " << endl;;
+
+  //init dice
+  int dice[diceside];
+  for (int i = 0; i < diceside; i++) {
+    dice[i] = i+1;
+  }
+
+  //init results
+  int results_sum = (diceside + diceside + 1);
+  int results [(diceside + diceside + 1)];
+  for (int i = 0; i < results_sum; i++) {
+      results[i] = 0;
+    }
+
+
+  int sum = 0;
+  int combinations = 0;
+
+  //write plus
+  for (int i = 0; i < diceside; i++) {
+    for (int j = 0; j < diceside; j++) {
       cout << dice[i] << " + " << dice[j] << " = " << (dice[i] + dice[j]) << endl;
       combinations++;
       sum = dice[i] + dice[j];
       results[sum]++;
     }
   }
-  cout << combinations << endl;
 
-  for (int i = 0; i < results_numb; i++) {
+  //write possibility
+  cout << "Lehetõségek száma:" << combinations << endl;
+  for (int i = 2; i < results_sum; i++) {
     cout << i << " -> " << results[i] << endl;
   }
 
-	return 0;
+  return 0;
 }
